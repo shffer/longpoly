bb7ed3d1fa29ab0a06ef313d04287a4b2b448b67
Shane Fernandez <s.fernandez@ecu.edu.au>
2025-03-07 14:42:21 +0800

    First save with manuals started

--- R/test_polynomial.R
+++ R/test_polynomial.R
@@ -1,11 +1,27 @@
+#' Test models with polynomials of differing orders to describe the relationship between linear rate of change and mean score in longitudinal data
+#'
+#' @param data Output from `longpoly::get_slopes_and_mean()` (or any tibble with columns "id", "performance_slope", & "performance_mean")
+#' @param test_proportion The proportion of participants to allocate to the test set. Default = `2/3`
+#' @param max_order The maximum order for polynomial models to be tested. Polynomial models for 1:`max_order` will be tested. Default = `6`
+#'
+#' @return
+#' A list containing:
+#' 1. `polynomial_results` A tibble with columns recording the order of each polynomial tested with the corresponding PVE in test and train data, as well as the additional PVE in the test data for the increase in order for each
+#' 2. `train_ids` A character vector of ids allocated to the train dataset in model development
+#' 3. `test_ids` A character vector of ids allocated to the test dataset in model development
+#' 4. `scree_plot` A visualisation of the additional PVE for each polynomial. It is recommended that the order of the model be select as that to the left of the ‘elbow’ (where the improvement plateaus).
+#' @export
+#'
+#' @examples
+#' #example()
+
 test_polynomial <-
   function(data,
            test_proportion = 2 / 3,
-           idcol = "id",
            max_order = 6) {
     library(ggplot2)
     set.seed(2244)
-
+    idcol = "id"
     # column names as expected from get_slopes_and_mean() output
     outcome <- "performance_slope"
     predictor <- "performance_mean"
